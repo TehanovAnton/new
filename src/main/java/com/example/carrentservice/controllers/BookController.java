be11fab5299128611d2730bf9333a1121d8c9f7d
antony <tehanovanton@gmail.com>
2021-12-08 16:08:39 +0300

    book, update car

--- src/main/java/com/example/carrentservice/controllers/BookController.java
+++ src/main/java/com/example/carrentservice/controllers/BookController.java
@@ -84,36 +84,37 @@ public class BookController {
         return new ModelAndView("EditBook");
     }
 
+
     @PostMapping(value="/book/update")
-    public ModelAndView check_dates(Model model,
-                                    @RequestParam(value="car_id") Long carId,
+    public ModelAndView editBook(Model model,
+                                    @RequestParam(value="car_id")
+                                            Long carId,
                                     @RequestParam(value="start_date") @DateTimeFormat(pattern = "yyyy-MM-dd")
                                             Calendar start_date,
                                     @RequestParam(value="end_date") @DateTimeFormat(pattern = "yyyy-MM-dd")
-                                                Calendar end_date,
-                                    @RequestParam("book_id") Long bookId) {
-
-        // validate ? redirect to edit again : redirect to show book
-        // but without validations just update
-
+                                             Calendar end_date,
+                                    @RequestParam("book_id")
+                                             Long bookId) {
         BorrowedDate borrowedDate = borrowedDateService.findById(bookId);
         borrowedDate.setStartDate(start_date);
         borrowedDate.setEndDate(end_date);
         borrowedDate.setCar(carService.findById(carId).get());
         borrowedDateService.save(borrowedDate);
 
-        return new ModelAndView("redirect:/customer/book/edit?book_id=" + bookId + "&start_date=" + format.format(start_date.getTime()) +
-                "&end_date=" + format.format(end_date.getTime()));
+        return new ModelAndView("redirect:/customer/books");
     }
 
-    @PostMapping(value = "/customer/book/edit{book_id}")
-    public ModelAndView editBook(Model model,
-                                 @RequestParam(value = "book_id") Long book_id,
-                                 @RequestParam(value = "car_id") Long carId,
-                                 @RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Calendar startDate,
-                                 @RequestParam(value = "end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Calendar endDate) {
-        return new ModelAndView("/customer/book/edit?book_id=" + book_id);
+
+    @PostMapping(value = "/check_dates")
+    public ModelAndView check_dates(Model model,
+                                    @RequestParam(value = "book_id") Long book_id,
+                                    @RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Calendar startDate,
+                                    @RequestParam(value = "end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Calendar endDate) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return new ModelAndView("redirect:/customer/book/edit?book_id=" + book_id +
+                "&start_date=" + format.format(startDate.getTime()) + "&end_date=" + format.format(endDate.getTime()));
     }
+
 
     private List<ShowBorrowedDate> showCustomerBooks(List<BorrowedDate> customerBorrowedDates, List<Car> customerBorrowedCars) {
         List<ShowBorrowedDate> dates = new ArrayList<ShowBorrowedDate>();
@@ -122,6 +123,7 @@ public class BookController {
         }
         return dates;
     }
+
 
     @GetMapping(value = "/bookPartOne{car_id}")
     public ModelAndView checkDates(Model model,
@@ -142,6 +144,7 @@ public class BookController {
         
         return new ModelAndView("bookPartOne");
     }
+
 
     @PostMapping(value = "/bookPartOne")
     public ModelAndView createNewCustomer(Model model,
